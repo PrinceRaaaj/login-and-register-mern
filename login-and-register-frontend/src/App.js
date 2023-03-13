375573c21a5b6233f3aa2ba1a9fd031a25e7a3ed
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2023-03-13 22:49:27 +0530

    Changes in App.js file : Switch to Routes

--- login-and-register-frontend/src/App.js
+++ login-and-register-frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import Homepage from "./components/homepage/homepage"
 import Login from "./components/login/login"
 import Register from "./components/register/register"
-import { BrowserRouter as Router,Route, Route } from "react-router-dom";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import { useState } from 'react';
 
 function App() {
